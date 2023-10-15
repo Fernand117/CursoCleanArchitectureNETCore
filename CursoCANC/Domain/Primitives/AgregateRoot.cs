34e3d82ce7e45480e4f1151447ff92157620b8cd
Luis Fernando Leyva Luna <fernando_rendel892@hotmail.com>
2023-10-15 10:12:30 -0600

    Configuración de Domain
    Se implementa un patrón llamado Aggregate, según Martín Fowler, es un patron en Domain-Drive Design

--- CursoCANC/Domain/Primitives/AgregateRoot.cs
+++ CursoCANC/Domain/Primitives/AgregateRoot.cs
@@ -2,5 +2,12 @@
 
 public abstract class AgregateRoot
 {
-    
+    private readonly List<DomainEvents> _domainEvents = new List<DomainEvents>();
+
+    public ICollection<DomainEvents> GetDomainEventsCollection() => _domainEvents;
+
+    protected void Raise(DomainEvents domainEvents)
+    {
+        _domainEvents.Add(domainEvents);
+    }
 }
