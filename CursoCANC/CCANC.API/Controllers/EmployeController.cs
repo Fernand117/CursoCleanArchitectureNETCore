585efd5a0fd8b33298953e623ad4e6fa8d43b5e2
Fernand117 <fernando_rendel892@hotmail.com>
2023-10-21 13:05:45 -0600

    feat(employe-controller-update): add the function update at employe controller

--- CursoCANC/CCANC.API/Controllers/EmployeController.cs
+++ CursoCANC/CCANC.API/Controllers/EmployeController.cs
@@ -1,3 +1,6 @@
+using Application.Employes.Create;
+using Application.Employes.Update;
+using ErrorOr;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,5 +15,31 @@ public class EmployeController : ApiController
     public EmployeController(ISender mediator)
     {
         _mediator = mediator ?? throw new ArgumentException(nameof(mediator));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateEmployeCommand command)
+    {
+        var createEmployeResult = await _mediator.Send(command);
+
+        return createEmployeResult.Match(employe => Ok(), errors => Problem(errors));
+    }
+
+    [HttpPut("id/{Id}")]
+    public async Task<IActionResult> Update(Guid Id, [FromBody] UpdateEmployeCommand command)
+    {
+        if (command.Id != Id)
+        {
+            List<Error> errors = new()
+            {
+                Error.Validation("Employe.UpdateInfo", "The request ID does not match with the url ID.")
+            };
+
+            return Problem();
+        }
+
+        var updateResult = await _mediator.Send(command);
+
+        return updateResult.Match(employeId => NoContent(), errors => Problem(errors));
     }
 }
